4ff3953fb7c6e4512f97856d4d6c8e58fdc21f1c
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-09-09 16:27:12 -0700

    convert km to mile

---
+++ 3-convertor.py
@@ -0,0 +1,11 @@
+'''programmer: Mahrokh Ebrahimi
+Description: user enter a number as a km. it will convert to mile
+Date: 9/9/2020'''
+
+kms = float(input('Enter a kilometer to convert it to mile: ') )
+
+mile = kms/1.6
+
+print(f'kilometer is {kms} and it is going to be {mile} miles')
+
+ 
