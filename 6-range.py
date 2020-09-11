3bfcfda33ced76bdf4b567eedcd6589560528483
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-09-11 12:56:19 -0700

    for loop

---
+++ 6-range.py
@@ -0,0 +1,10 @@
+for num in range(1, 10):
+    print('*' * num)
+
+print('==================================')
+
+
+for j in range(10):
+    if j%2!=0:
+        for m in range(6):
+            print('*' * m)
