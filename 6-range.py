9aa783300433a63c7a66a708c0644f466cf20dd4
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-09-11 21:17:29 -0700

    for Loop

--- 6-range.py
+++ 6-range.py
@@ -11,7 +11,14 @@ for j in range(10):
     else:
         for m in range(5, 0, -1):
             print('*' * m)
+            
+print('==================================')
 
+for i in range(10):
+    star = ''
+    for j in range(i):
+        star = star + '*'
+        print(star)
 
     
 
