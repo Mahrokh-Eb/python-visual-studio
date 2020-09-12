59a5336e144f510d6b45d8189de9a06b3265c1f0
Mahrokh Ebrahimi <mahrokh@Mahrokhs-MacBook-Pro-2.local>
2020-09-12 14:13:00 -0700

    list Functions

---
+++ 8-list-function.py
@@ -0,0 +1,10 @@
+#adding one course to list
+myCourses = ['python', 'kotlin', 'Ionic']
+
+myCourses.append('JQuery')
+
+print(myCourses)
+
+# adding many different items to list
+myCourses.extend(['x', 'y'])
+print(myCourses)
